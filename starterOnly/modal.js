f2f4beb3f222dbf4829c7ecdd0e292e8ba43ae75
ZhannaZucher <jzucher@gmail.com>
2023-01-10 18:48:09 +0100

    fix :  reorganize  inputs' validation functions

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -14,6 +14,8 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeBtn = document.querySelectorAll(".close");
+const form = document.getElementsByTagName('form')[0];
+const submitBtn = document.getElementsByClassName('btn-submit')[0];
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -32,28 +34,13 @@ function closeModal() {
 closeBtn.forEach((btn) => btn.addEventListener("click", closeModal));
 
 
-//Function for input's values validation and errors alerting
-
-
-function validateForm() {
-
+/******Functions for input's values validation and errors alerting*****/
+// Firstname input value validating function
+function validateFirstName(formIsValid) {
   const firstName = document.getElementById('first');
-  const lastName = document.getElementById('last');
-  const email = document.getElementById('email');
-  const birthDate = document.getElementById('birthdate');
-  const contestQuantity = document.getElementById('quantity');
-  const checkboxTerms = document.getElementById('checkbox1');
-  const loc1 = document.getElementById('location1');
-  const loc2 = document.getElementById('location2');
-  const loc3 = document.getElementById('location3');
-  const loc4 = document.getElementById('location4');
-  const loc5 = document.getElementById('location5');
-  const loc6 = document.getElementById('location6');
-  let formIsValid = true;
-
   //if the input value is empty or less of 2 characters 
   if (firstName.value === '' || firstName.value.length < 2) {
-  //show this error message
+    //show this error message
     document
       .querySelector('#error-firstName')
       .textContent = 'Veuillez renseigner votre prénom.';
@@ -62,10 +49,24 @@ function validateForm() {
       .setAttribute('data-error-visible', 'true');
     formIsValid = false;
   }
+  else {
+    // Remove error alert
+    document
+      .querySelector('#error-firstName')
+      .textContent = '';
+    firstName
+      .closest(".formData")
+      .removeAttribute('data-error-visible');
+  }
+  return formIsValid;
+};
 
-  //if the input value is empty or less of 2 characters 
+// Lastname input value validating function
+function validateLastName(formIsValid) {
+  const lastName = document.getElementById('last');
+  // if the input value is empty or less of 2 characters 
   if (lastName.value === '' || lastName.value.length < 2) {
-  //show this error message
+    // show this error message
     document
       .querySelector('#error-lastName')
       .textContent = 'Veuillez renseigner votre nom.';
@@ -74,10 +75,24 @@ function validateForm() {
       .setAttribute('data-error-visible', 'true');
     formIsValid = false;
   }
+  else {
+    // Remove error alert
+    document
+      .querySelector('#error-lastName')
+      .textContent = '';
+    lastName
+      .closest(".formData")
+      .removeAttribute('data-error-visible');
+  }
+  return formIsValid;
+};
 
+// Email input value validating function
+function validateEmail(formIsValid) {
+  const email = document.getElementById('email');
   //if the input value is missing or the value does not match with requiered via 'type' attribute
   if (!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(email.value)) {
-  //show this error message
+    //show this error message
     document
       .querySelector('#error-email')
       .textContent = 'Veuillez renseigner votre email.';
@@ -86,10 +101,24 @@ function validateForm() {
       .setAttribute('data-error-visible', 'true');
     formIsValid = false;
   }
+  else {
+    // Remove error alert
+    document
+      .querySelector('#error-email')
+      .textContent = '';
+    email
+      .closest(".formData")
+      .removeAttribute('data-error-visible');
+  }
+  return formIsValid;
+};
 
-  //if the birthdate value is missing
+// Birthdate input value validating function
+function validateBirthDate(formIsValid) {
+  const birthDate = document.getElementById('birthdate');
+  // if the birthdate value is missing
   if (birthDate.value === '') {
-  //show this error message
+    // Show this error message
     document
       .querySelector('#error-birthdate')
       .textContent = 'Veuillez renseigner votre date de naissance.';
@@ -98,10 +127,24 @@ function validateForm() {
       .setAttribute('data-error-visible', 'true');
     formIsValid = false;
   }
+  else {
+  // Remove error alert
+    document
+      .querySelector('#error-birthdate')
+      .textContent = '';
+    birthDate
+      .closest(".formData")
+      .removeAttribute('data-error-visible');
+  }
+  return formIsValid;
+};
 
-  //if the input value is missing or the value does not match with requiered via type attribute
+// Quantity of contests made input value validating function
+function validateContestQuantity(formIsValid) {
+  const contestQuantity = document.getElementById('quantity');
+  // if the input value is missing or the value does not match with requiered via type attribute
   if (contestQuantity.value === '' || contestQuantity.value.typeMismatch) {
-  //show this error message
+  // Show this error message
     document
       .querySelector('#error-contest')
       .textContent = 'Veuillez renseigner le nombre de tournois.';
@@ -110,35 +153,86 @@ function validateForm() {
       .setAttribute('data-error-visible', 'true');
     formIsValid = false;
   }
+  else {
+  // Remove error alert
+    document
+      .querySelector('#error-contest')
+      .textContent = '';
+    contestQuantity
+      .closest(".formData")
+      .removeAttribute('data-error-visible');
+  }
+  return formIsValid;
+};
 
-  //if the requiered terms are not accepted
+// Checkbox for requiered terms approval validating function
+function validateCheckBoxTerms(formIsValid) {
+  const checkboxTerms = document.getElementById('checkbox1');
+  // if the requiered terms are not accepted
   if (!checkboxTerms.checked) {
-  //show this error message
+  // Show this error message
     document
       .querySelector('#error-terms')
       .textContent = 'Veuillez accepter les conditions d\'utilisation.';
     formIsValid = false;
   }
- //if no one checkbox is checked
+  else {
+  // Remove error alert
+    document
+      .querySelector('#error-terms')
+      .textContent = '';
+  }
+  return formIsValid;
+};
+
+//Function for location checkbox' input's values validation and errors alerting:
+function validateCheckBoxLocations(formIsValid) {
+  const loc1 = document.getElementById('location1');
+  const loc2 = document.getElementById('location2');
+  const loc3 = document.getElementById('location3');
+  const loc4 = document.getElementById('location4');
+  const loc5 = document.getElementById('location5');
+  const loc6 = document.getElementById('location6');
+  //if no one checkbox is checked
   if (!loc1.checked && !loc2.checked && !loc3.checked && !loc4.checked && !loc5.checked && !loc6.checked) {
-  //show this error message
+    //show this error message
     document
       .querySelector('#error-location')
       .textContent = 'Veuillez choisir une ville.';
     formIsValid = false;
   }
+  else {
+  // Remove error alert
+    document
+      .querySelector('#error-location')
+      .textContent = '';
+  }
+  return formIsValid;
+};
 
+
+//Function for form validation
+function validateForm() {
+  let formIsValid = true;
+  formIsValid = validateFirstName(formIsValid);
+  formIsValid = validateLastName(formIsValid);
+  formIsValid = validateEmail(formIsValid);
+  formIsValid = validateBirthDate(formIsValid);
+  formIsValid = validateContestQuantity(formIsValid);
+  formIsValid = validateCheckBoxTerms(formIsValid);
+  formIsValid = validateCheckBoxLocations(formIsValid);
   return formIsValid;
 }
 
-// 
-const form = document.getElementsByTagName('form')[0];
-const submitBtn = document.getElementsByClassName('btn-submit')[0];
 
-submitBtn.addEventListener('click', function (event) {
-  if (!validateForm()) {
-    event.preventDefault();
-  } else {
+//submit modal event
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  if (validateForm()) {
     alert ('ok');
   }
 });
+
+
+
+
